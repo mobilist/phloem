3bc7a8746acf623d5c8934a6fb39b9e669446880
sconedog <sconedog@localhost>
2009-08-29 17:30:48 +0000

    Tidied up the Phloem constants.

--- lib/Phloem/Constants.pm
+++ lib/Phloem/Constants.pm
@@ -39,13 +39,12 @@ This file is part of Phloem.
 
 package Phloem::Constants;
 
-our $LOG_FILE                        = 'phloem.log';
-our $CONFIG_FILE                     = 'etc/node.xml';
+our $LOG_FILE                       = 'phloem.log';
+our $CONFIG_FILE                    = 'etc/node.xml';
 
-our $REGISTRY_SERVER_TIMEOUT_S       = 30;
+our $REGISTRY_SERVER_TIMEOUT_S      = 30;
 
-our $SUBSCRIBER_UPDATE_SLEEP_TIME_S  = 10;
-our $NODE_REGISTER_SLEEP_TIME_S = 10;
-
+our $SUBSCRIBER_UPDATE_SLEEP_TIME_S = 10;
+our $NODE_REGISTER_SLEEP_TIME_S     = 10;
 
 1;
