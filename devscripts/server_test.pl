fb88b3941f87fb2644cd2365fa335c1a27d8944e
sconedog <sconedog@localhost>
2009-09-02 14:15:35 +0000

    Refactored the logging code.

--- devscripts/server_test.pl
+++ devscripts/server_test.pl
@@ -16,8 +16,8 @@ use Phloem::Root;
 {
   Phloem::Debug->enabled(1);
   Phloem::Logger->initialise();
-  Phloem::Logger::clear();
-  Phloem::Logger::append('Starting up.');
+  Phloem::Logger->clear();
+  Phloem::Logger->append('Starting up.');
   my $root = Phloem::Root->new('host' => '10.127.10.4', 'port' => 9999);
   Phloem::RegistryServer->run($root);
 }
